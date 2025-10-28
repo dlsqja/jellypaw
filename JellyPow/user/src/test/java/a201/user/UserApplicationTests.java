ded593d37e7a8f891827b7141e485b014fc7dbb5
송인범 <dlsqja3436@gmail.com>
2025-10-28 10:16:50 +0900

    add: user 모듈 추가

--- JellyPow/user/src/test/java/a201/user/UserApplicationTests.java
+++ JellyPow/user/src/test/java/a201/user/UserApplicationTests.java
@@ -1,13 +1,13 @@
-package com.A201.JellyPow;
+package a201.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JellyPowApplicationTests {
+class UserApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
