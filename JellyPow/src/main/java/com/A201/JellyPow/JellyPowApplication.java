925fb656e500513322a2b5238c5f65c2fead4417
송인범 <dlsqja3436@gmail.com>
2025-10-28 09:56:01 +0900

    release 프로젝트 생성

---
+++ JellyPow/src/main/java/com/A201/JellyPow/JellyPowApplication.java
@@ -0,0 +1,13 @@
+package com.A201.JellyPow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JellyPowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JellyPowApplication.class, args);
+	}
+
+}
---
+++ JellyPow/src/test/java/com/A201/JellyPow/JellyPowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.A201.JellyPow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JellyPowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
