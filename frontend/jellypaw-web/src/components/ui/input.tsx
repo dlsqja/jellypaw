7f4bd4cc1eaea8df05731b933ac64d9ed848283e
daeyeonlee <angellee98@naver.com>
2025-10-29 17:12:19 +0900

    feat : input 컴포넌트 추가, search 타입 수정

---
+++ frontend/jellypaw-web/src/components/ui/input.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+
+import { cn } from '@/lib/utils';
+import { IoSearch } from 'react-icons/io5';
+
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(({ className, type, ...props }, ref) => {
+  const inputElement = (
+    <input
+      type={type}
+      className={cn(
+        'flex h-12 rounded-2xl border border-input bg-white-100 py-3 p2 placeholder:text-gray-300 placeholder:p2 focus:outline-none focus-visible:outline-none focus-visible:ring-0',
+        type === 'search'
+          ? 'w-full pl-12 pr-3 [&::-webkit-search-cancel-button]:appearance-none [&::-webkit-search-cancel-button]:hidden [&::-ms-clear]:hidden'
+          : 'w-full px-4',
+        className,
+      )}
+      ref={ref}
+      {...props}
+    />
+  );
+
+  if (type === 'search') {
+    return (
+      <div className="relative w-full">
+        <IoSearch className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-300 pointer-events-none" size={20} />
+        {inputElement}
+      </div>
+    );
+  }
+
+  return inputElement;
+});
+Input.displayName = 'Input';
+
+export { Input };
--- frontend/jellypaw-web/src/pages/Feed/Feed.tsx
+++ frontend/jellypaw-web/src/pages/Feed/Feed.tsx
@@ -1,6 +1,6 @@
 import BackHeader from '@/components/headers/BackHeader';
 import Header from '@/components/headers/Header';
-
+import { Input } from '@/components/ui/input';
 export default function Feed() {
   return (
     <div>
@@ -8,6 +8,14 @@ export default function Feed() {
       <Header title="동물관리" />
       <Header title="검색" />
       <Header title="내 공간" />
+
+      <Input type="search" placeholder="검색어 입력" />
+      <Input type="text" placeholder="텍스트" />
+      <Input type="email" placeholder="이메일" />
+      <Input type="password" placeholder="비밀번호" />
+      <Input type="number" placeholder="숫자" />
+      <Input type="tel" placeholder="전화번호" />
+      <Input type="url" placeholder="URL" />
     </div>
   );
 }
