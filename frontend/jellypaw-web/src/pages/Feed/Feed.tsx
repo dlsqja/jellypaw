0e4c9cae9a55a8a7eb453835a4e9eec0baa79b39
daeyeonlee <angellee98@naver.com>
2025-10-30 09:33:11 +0900

    feat : 팔로워 목록 컴포넌트 추가

--- frontend/jellypaw-web/src/pages/Feed/Feed.tsx
+++ frontend/jellypaw-web/src/pages/Feed/Feed.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import Header from '@/components/headers/Header';
 import Followers from '@/pages/Feed/Followers';
-
+import { FiUsers } from 'react-icons/fi';
 export default function Feed() {
+  // 팔로워 목록
   const petProfiles = [
     { name: '탄산', imageUrl: '/src/assets/pets/반려동물1.png' },
     { name: '구찌', imageUrl: '/src/assets/pets/반려동물2.png' },
@@ -12,7 +13,7 @@ export default function Feed() {
     { name: '뽕따', imageUrl: '/src/assets/pets/반려동물3.png' },
   ];
 
-  const [activeProfile, setActiveProfile] = useState<string>('탄산');
+  const [activeProfile, setActiveProfile] = useState<string>('전체');
 
   const handleProfileClick = (name: string) => {
     setActiveProfile(name);
@@ -21,14 +22,31 @@ export default function Feed() {
   return (
     <>
       <Header title="피드" />
-      <div className="flex overflow-x-auto gap-4 p-4 w-full items-center">
+
+      {/* 팔로워 목록 */}
+      <div className="flex overflow-x-auto gap-4 w-full h-[95px] items-center scrollbar-hide">
+        {/* 전체 */}
+        <div className="w-16 h-20 flex flex-col gap-2 items-center cursor-pointer" onClick={() => setActiveProfile('전체')}>
+          <div
+            className="w-16 h-16 p-2 rounded-full outline outline-2 outline-offset-[-2px]
+              outline-aqua-300 flex flex-col justify-center items-center"
+          >
+            <div className="w-12 h-12 bg-aqua-300 rounded-full inline-flex justify-center items-center">
+              <FiUsers size={24} color="#ffffff" />
+            </div>
+          </div>
+          <div className={`text-center p3-b ${activeProfile === '전체' ? 'text-aqua-300' : 'text-gray-300'}`}>전체</div>
+        </div>
         {petProfiles.map((petProfile) => (
           <Followers
             key={petProfile.name}
             imageUrl={petProfile.imageUrl || ''}
             name={petProfile.name}
             isActive={activeProfile === petProfile.name}
-            onClick={() => handleProfileClick(petProfile.name)}
+            onClick={() => {
+              setActiveProfile(petProfile.name);
+              handleProfileClick(petProfile.name);
+            }}
           />
         ))}
       </div>
--- frontend/jellypaw-web/src/pages/Feed/Followers.tsx
+++ frontend/jellypaw-web/src/pages/Feed/Followers.tsx
@@ -10,13 +10,13 @@ export default function Followers({ imageUrl, name, isActive = false, onClick }:
   const textColor = isActive ? 'text-aqua-300' : 'text-gray-300';
 
   return (
-    <div className="w-16 h-20 flex-shrink-0 inline-flex flex-col justify-start items-center cursor-pointer" onClick={onClick}>
-      <div className={`w-16 h-16 p-1.5 rounded-full outline outline-2 outline-offset-[-2px] ${outlineColor} flex flex-col justify-start items-start`}>
+    <div className="w-16 h-20 flex flex-col gap-2 items-center cursor-pointer" onClick={onClick}>
+      <div className={`w-16 h-16 p-2 rounded-full outline outline-2 outline-offset-[-2px] ${outlineColor} flex flex-col justify-start items-start`}>
         <img className="w-12 h-12 rounded-full object-cover" src={imageUrl} alt={name} />
       </div>
 
-      <div className="w-full flex justify-center items-center pt-2">
-        <div className={`text-center p2 ${textColor} leading-5`}>{name}</div>
+      <div className="w-full flex justify-center">
+        <div className={`text-center p3-b ${textColor}`}>{name}</div>
       </div>
     </div>
   );
