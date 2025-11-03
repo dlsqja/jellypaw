3d37e7c41156c84df19bacad448ca4a2145588d9
송인범 <dlsqja3436@gmail.com>
2025-11-03 17:38:35 +0900

    fix: url boards 추가

--- JellyPow/board/src/main/java/a201/board/controller/LikeController.java
+++ JellyPow/board/src/main/java/a201/board/controller/LikeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/likes")
+@RequestMapping("/boards/likes")
 @RequiredArgsConstructor
 public class LikeController {
 
