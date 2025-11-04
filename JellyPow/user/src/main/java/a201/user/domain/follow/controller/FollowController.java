c6f8cb340fb4212d685c0f02fc6f272072d8ff6b
김유성 <usk05@daum.net>
2025-11-04 10:54:28 +0900

    fix: api url 수정

--- JellyPow/user/src/main/java/a201/user/domain/follow/controller/FollowController.java
+++ JellyPow/user/src/main/java/a201/user/domain/follow/controller/FollowController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/user/follow")
+@RequestMapping("/users/follow")
 @RequiredArgsConstructor
 public class FollowController {
 
