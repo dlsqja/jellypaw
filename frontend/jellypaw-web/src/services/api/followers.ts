52ce909b433cbe0c5e7af26cb0484098e6986364
daeyeonlee <angellee98@naver.com>
2025-11-06 13:32:55 +0900

    feat : 팔로워 목록 조회 API 추가

--- frontend/jellypaw-web/src/services/api/followers.ts
+++ frontend/jellypaw-web/src/services/api/followers.ts
@@ -2,6 +2,6 @@
 import apiClient from '@/lib/axios';
 
 export const getFollowers = async () => {
-  const response = await apiClient.get(`users/follow/followers`);
+  const response = await apiClient.get(`users/follow/followers/카미나리로코큐`);
   return response.data;
 };
