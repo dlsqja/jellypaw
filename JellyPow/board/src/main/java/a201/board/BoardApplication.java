0ed75e11f5e6e5abe03a801627756fd17fa2d121
안성수 <sungsoo6414@naver.com>
2025-11-12 18:34:35 +0900

    feat : push

--- JellyPow/board/src/main/java/a201/board/BoardApplication.java
+++ JellyPow/board/src/main/java/a201/board/BoardApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @Import(CommonModuleConfig.class)
 public class BoardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BoardApplication.class, args);
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }
