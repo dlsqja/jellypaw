77ead503789f22141aa7925ade69362491e54ebc
daeyeonlee <angellee98@naver.com>
2025-11-13 13:11:12 +0900

    style : 프로필 편집 화면 모바일과 동일하게 수정

--- frontend/jellypaw-web/src/components/ui/textarea.tsx
+++ frontend/jellypaw-web/src/components/ui/textarea.tsx
@@ -6,7 +6,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'tex
   return (
     <textarea
       className={cn(
-        'flex h-25 w-full rounded-[12px] border border-gray-200 bg-white-100 px-4 py-3 p2 focus:outline-none focus-visible:outline-none focus-visible:ring-0 resize-none',
+        'flex h-25 w-full rounded-[12px] border border-gray-200 bg-white-100 px-4 py-3 p2 focus:outline-none focus-visible:outline-none focus-visible:border-aqua-300 focus-visible:ring-0.5 focus-visible:ring-aqua-300 resize-none',
         '[&::placeholder]:text-gray-300 [&::placeholder]:text-sm [&::placeholder]:leading-[1.5] [&::placeholder]:font-normal',
         className,
       )}
