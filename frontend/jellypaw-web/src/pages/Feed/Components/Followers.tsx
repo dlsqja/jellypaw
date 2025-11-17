656617540769a135e1d7cb9297d35ae36c19c5d8
daeyeonlee <angellee98@naver.com>
2025-11-17 22:16:11 +0900

    fix : 팔로워 프로필 이미지 비율 수정

--- frontend/jellypaw-web/src/pages/Feed/Components/Followers.tsx
+++ frontend/jellypaw-web/src/pages/Feed/Components/Followers.tsx
@@ -14,11 +14,11 @@ export default function Followers({ imageUrl, name, isActive = false, onClick }:
   return (
     <div className="w-16 h-20 flex flex-col gap-2 items-center cursor-pointer" onClick={onClick}>
       <div
-        className={`w-16 h-16 p-1.5 rounded-full outline outline-2 outline-offset-[-2px] ${outlineColor} flex flex-col justify-center items-center`}
+        className={`w-16 h-16 p-1.5 relative rounded-full outline outline-2 outline-offset-[-2px] ${outlineColor} flex flex-col justify-center items-center`}
       >
         {/* 프로필 이미지 없으면 기본 이미지 */}
         {imageUrl ? (
-          <img className="w-14 h-14 rounded-full object-cover" src={imageUrl} alt={name} />
+          <img className="w-full h-full rounded-full object-cover" src={imageUrl} alt={name} />
         ) : (
           <div
             className={`w-16 h-16 p-4 rounded-full outline outline-2 outline-offset-[-2px] ${
